fecda91c294bea13a78edd5f1e97b4eadaac0bed
vuquangnhat <nhatchien12@gmail.com>
2024-04-21 13:07:12 +0700

    Update: use unordered_map instead of map, add the condition to check if the value n is calculated

--- Home_work/baitaptuluyen/bai1.cpp
+++ Home_work/baitaptuluyen/bai1.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 
 #define ll long long
-map<ll, ll> dp;
+unordered_map<ll, ll> dp;
 
 ll f(ll n) {
     if (n == 1ll)
         return dp[n] = 0;
+    if (dp.count(n)) // Nếu n đã được tính toán trước đó
+        return dp[n];
     if (n & 1)
         dp[n] = 1ll + f(3*n + 1ll);
     else
