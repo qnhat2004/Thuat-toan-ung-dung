b54e6dfe710c1dca4a588b3bb50a78d36d3d9354
vuquangnhat <nhatchien12@gmail.com>
2024-05-10 08:01:22 +0700

    'Lat gach duong doi' has done and add it to the list

--- Finetest/Lat-gach-duong-doi.cpp
+++ Finetest/Lat-gach-duong-doi.cpp
@@ -5,11 +5,12 @@ int main()
 {
     int n;
     cout << "N = "; cin >> n;
-    int a = 1, b = 1;
-    for (int i = 1; i <= n; ++i) {
-        b = 2*a + b;
-        a = b - 2*a;
-    }
+    int a = 1, b = 1, c;
+    for (int i = 2; i <= n; ++i) {
+        c = 2*a + b;
+        a = b;
+        b = c;
+    };
     cout << "So phuong an = " << b;
     return 0;
 }
