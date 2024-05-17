6c9cfdd9127111162d8e2fc6bae2d1c739a22ff5
vuquangnhat <nhatchien12@gmail.com>
2024-05-17 07:52:49 +0700

    Đã làm xong - AC

--- Finetest/Phan-tich-N-thanh-tong-M-so.cpp
+++ Finetest/Phan-tich-N-thanh-tong-M-so.cpp
@@ -1,18 +1,31 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 int m, n;
+int tmp[100];
 
-void gen(int i) {
-    
+void print() {
+	cout << n << " = " << tmp[0];
+	for (int i = 1; i < m; i++)
+		cout << "+" << tmp[i];
+	cout << endl;
 }
-
-int main()
-{
-    
-    cout << "Nhap m = "; cin >> m;
-    cout << "Nhap n = "; cin >> n;
-
-
-    return 0;
+void gen(int i, int sum) {
+	if (i == m) {
+		if (sum == n) {
+			print();
+		}
+		return;
+	}
+	for (int j = 1; j <= n; j++) {
+		if (sum + j > n) continue;
+		tmp[i] = j;
+		gen(i+1, sum+j); 
+	}
+}
+int main(){
+	cout << "Nhap n = "; cin >> n;
+	cout << "Nhap m = "; cin >> m;
+	gen(0, 0);	
+	return 0;
 }
