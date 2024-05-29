075c245b1787d7e3bcad3cab2a53e6b5d512187e
quang nhat <nhatchien12@gmail.com>
2024-05-29 22:30:10 +0700

    create

--- Home_work/Dequy-Quaylui-Nhanhcan/Bai2.cpp
+++ Home_work/Dequy-Quaylui-Nhanhcan/Bai2.cpp
@@ -1,11 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
+int n, a[100];
+
+void print() {
+    for (int i = 0; i < n; i++) cout << a[i] << "";
+    cout << endl;
+}
+
+void gen(int k) {
+    if (k >= n) {
+        print();
+        return;
+    }
+    for (int i = 0; i <= 2; i++) {
+        a[k] = i;
+        gen(k + 1);
+    }
+}
 
 int main()
 {
     cout << "Nhap n = ";
     cin >> n;    
+    gen(0);
     return 0;
 }
