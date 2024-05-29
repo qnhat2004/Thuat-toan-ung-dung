9367aa5b0363b5c27bcdadbd529bbaa4a738b2e5
quang nhat <nhatchien12@gmail.com>
2024-05-29 10:54:10 +0700

    update

--- Home_work/Tham-Lam/Bai3.cpp
+++ Home_work/Tham-Lam/Bai3.cpp
@@ -1,29 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
     cout << "Nhap N = ";
     cin >> n;
-    int a[10] = {0}, i = 9;
-    while(n > 1 && i > 1) {
-        while(n % i == 0) {
-            a[i]++;
+    string s = "";
+    for (int i = 9; i > 1 && n > 1; i--)
+    {
+        while (n % i == 0)
+        {
+            s = to_string(i) + s; // or s = char(i + '0) + s;
             n /= i;
         }
-        i--;
     }
-    if (n == 0)
-        cout << "K = " << 10;
-    else if (n != 1) 
+    if (n == 1)
+        cout << "K = " << s;
+    else
         cout << "NO";
-    else {
-        cout << "K = ";
-        for (int i = 2; i <= 9; i++) {
-            for (int j = 0; j < a[i]; j++) {
-                cout << i;
-            }
-        }
-    }
     return 0;
 }
