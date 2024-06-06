d7075982919eb86c620d37cef342f244fff84c1f
quang nhat <nhatchien12@gmail.com>
2024-06-07 02:45:34 +0700

    Update: làm bằng mảng 2 chiều

--- DeBai/2-Phan-tich-n-thanh-tong-cac-so-duong.cpp
+++ DeBai/2-Phan-tich-n-thanh-tong-cac-so-duong.cpp
@@ -10,18 +10,18 @@ int gen(int i, int sz, int last) {
     	if (sz > 1) return 1;
 		return 0; 
 	}
-    int key = i*1000 + last;
-	if (dp.count(key)) return dp[key];
-	dp[key] = 0;
+	if (dp2[i][last] != -1)
+        return dp2[i][last];
+	dp2[i][last] = 0;
 	for (int j = min(i, last); j >= 1; j--) {
-		dp[key] += gen(i-j, sz+1, j);
+		dp2[i][last] += gen(i-j, sz+1, j);
 	}
-	return dp[key];
+	return dp2[i][last];
 }
 
 int main()
 {
-	// memset(dp, -1, sizeof(dp));
+	memset(dp2, -1, sizeof(dp2));
     cout << "nhap n\n";
     cin >> n;
 	cout << "So kieu phan tich la: " << gen(n, 0, n);
