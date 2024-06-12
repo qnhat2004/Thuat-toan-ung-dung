7bb4620580dd0b860f534dc2da4a55c34d396345
quang nhat <nhatchien12@gmail.com>
2024-06-12 07:00:57 +0700

    update

--- Finetest/Phan-tich-K-thanh-tong-cac-so-duong-khac-nhau.cpp
+++ Finetest/Phan-tich-K-thanh-tong-cac-so-duong-khac-nhau.cpp
@@ -7,8 +7,7 @@ int gen(int i, int sum) {
 	if (sum == k) return 1;
 	if (i == k) return 0;
 	if (dp[i][sum] != -1) return dp[i][sum];
-	dp[i][sum] = gen(i+1, sum) + gen(i+1, sum+i);
-	return dp[i][sum]; 
+	return dp[i][sum] = gen(i + 1, sum) + gen(i + 1, sum + i);
 }
 
 int main()
