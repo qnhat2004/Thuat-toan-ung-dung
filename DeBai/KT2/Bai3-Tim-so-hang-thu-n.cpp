d1a24a34f51560a65591ce703df7f9443d8bd383
quang nhat <nhatchien12@gmail.com>
2024-06-13 22:18:28 +0700

    done

--- DeBai/KT2/Bai3-Tim-so-hang-thu-n.cpp
+++ DeBai/KT2/Bai3-Tim-so-hang-thu-n.cpp
@@ -11,11 +11,24 @@ int loop() {
 		for (int j = i; j >= 1; j--) {
 			dp[i] += j*j * dp[i-j];
 		}
-		cout << "dp[" << i << "] = " << dp[i] << endl;
+//		cout << "dp[" << i << "] = " << dp[i] << endl;
 	}
 	return dp[n];
 }
+
+// Cach 2: dung de quy co nho
+int rec(int i) {
+    if (i == 0) return 1;
+    if (dp[i] != 0) return dp[i];
+    for (int j = i; j >= 1; j--) {
+    	dp[i] += j*j * rec(i-j);
+	}
+	return dp[i];
+}
+
 int main() {
 	cin >> n;
-	cout << loop();
+	cout << loop() << endl;
+    memset(dp, 0, sizeof(dp));
+    cout << rec(n) << endl;
 }
